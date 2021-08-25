9f3b6c57a2e12ef24cc90cd5bad1db399c4a63c7
Jefferson Douglas <jeffersken.06@gmail.com>
2021-08-25 12:10:01 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/jeffdoug/cursomc/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.jeffdoug.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jeffdoug.cursomc.domain.Cliente;
+import com.jeffdoug.cursomc.repositories.ClienteRepository;
+import com.jeffdoug.cursomc.services.exceptions.ObjectNotFoundException;
+
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id) throws ObjectNotFoundException {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+
+}
